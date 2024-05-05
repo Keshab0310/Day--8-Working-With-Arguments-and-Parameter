1277a82b244a43b1c5e0c2cc41522cb013445321
Keshab Kumar Pathak <156252352+Keshab0310@users.noreply.github.com>
2024-05-05 01:55:37 -0400

    The fully functional Ceaser Cipher which doesn't exit from terminal until the command is given

--- Ceaser Cipher.py
+++ Ceaser Cipher.py
@@ -1,36 +1,46 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        for char in start_text:
+            position = alphabet.index(char)
+            new_position = position - shift_amount
+            if new_position < 0:
+                new_letter = alphabet [new_position % 26]
+            else:
+                new_letter = alphabet[new_position]
+            end_text += new_letter
+          
+    else:
+       for char in start_text:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            if new_position > 25:
+                new_letter = alphabet [new_position % 26]
+            else:
+                new_letter = alphabet[new_position]
+            end_text += new_letter
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
-should_continue = True
-def encrypt(plain_text, shift_ammount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_ammount
-        if new_position > 25:
-            new_letters = alphabet[new_position % 26]
-        else:
-            new_letters = alphabet[new_position]
-        cipher_text += new_letters
-    print(f"The encoded text is {cipher_text}")
-def decrypt (new_text, new_shift):
-    original_text = ""
-    for letter in new_text:
-        positions = alphabet.index(letter)
-        new_positions = positions - new_shift
-        if new_positions < 0:
-            new_letter = alphabet [new_positions % 26]
-        else:
-            new_letter = alphabet[new_positions]
-        original_text += new_letter
-    print(f"The decoded text is {original_text}")
+from art import logo
+print(logo)
 
 
+should_end = False
+while not should_end:
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-if direction == "encode":
-    encrypt(plain_text=text, shift_ammount=shift)
-elif direction == "decode":
-    decrypt(new_text=text, new_shift=shift)
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+    
+
+
